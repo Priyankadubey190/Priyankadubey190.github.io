5586f22927c70716a3b4236cbf4f70f07771c1e8
Priyankadubey190 <priyankadubey0811@gmail.com>
2022-12-20 09:13:12 +0530

    latest commit

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import style from "./contact.module.css";
 import {AiFillGithub, AiFillLinkedin} from "react-icons/ai"
 export default function Contact() {
+
+  //https://www.linkedin.com/in/priyankadubey-dev/
+
   return (
     <div id="contact" className={style.concontainer}>
      <center>
         <h1 style={{fontSize:"40px"}}>Contact</h1>
         <h1>Feel free to get in touch with me via the following channels:</h1>
         <div className={style.flex}>
-          <AiFillGithub />  
-          <AiFillLinkedin />
+          <AiFillGithub onClick={()=>{window.open("https://github.com/Priyankadubey190","_blank")}} />  
+          <AiFillLinkedin onClick={()=>{window.open("https://www.linkedin.com/in/priyankadubey-dev/","_blank")}} />
         </div>
         <h1>Email</h1>
         <h2>priyankadubey0811@gmail.com</h2>
