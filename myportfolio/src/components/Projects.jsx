16fdca43aabfc889363256cb56f44c4818bd8ddd
Priyankadubey190 <priyankadubey0811@gmail.com>
2023-01-18 13:51:30 +0530

    final one project

--- myportfolio/src/components/Projects.jsx
+++ myportfolio/src/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects =()=> {
         <div>
             
             <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1527).png" alt="" width={"100%"}/>
-            <center className={style.cen}>Developers Conference</center>
+            <center className={style.cen}>Solo e-commerce</center>
             <p> This is a solo e-commerce website. In this website I used HTML css javaSctipe React for frontend for backend Node express mongoDB mongoose</p>
             <div className={style.probtn}>
             <a href="https://my-solo-full-ecommerce.netlify.app/">Live Site</a>
@@ -17,8 +17,8 @@ const Projects =()=> {
         </div>
         <div>
         <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1529).png" alt=""  width={"100%"}/>
-            <center className={style.cen}>Reebok-Clone</center>
-            <p>I did this Project in first construct week in Masai this is our team Project this is the e-commers site for designing in this project we used Html5,Css3,Javascript and to stored data we used local storage.</p>
+            <center className={style.cen}>Animoto-Clone</center>
+            <p>This is solo project in this website for front end I used HTML css javaScript and for backend I used JSON server.</p>
             <div>
             <a href="https://animoto-website.netlify.app/">Live Site</a>
             <a href="https://github.com/Priyankadubey190/innate-earth-4569" className={style.apro}>View Repo</a>
@@ -26,8 +26,8 @@ const Projects =()=> {
         </div>
         <div>
         <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1532).png" alt=""  width={"100%"}/>
-            <center className={style.cen}>Pai-international-Clone</center>
-            <p>This is my third construct week Project. There are six member in our team for designing we used HTML5, CSS3, JAVASCRIPT and for backend we used EXPRESS.JS, MongoDB Database.</p>
+            <center className={style.cen}>Nature's Basket-Clone</center>
+            <p>This is the clone of nature's basket website, we can use this website to buy fresh food item.</p>
             <div>
             <a href="https://naturesbaskett.vercel.app/">Live Site</a>
             <a href="https://github.com/JatinKhatter07/wiggly-bucket-6915" className={style.apro}>View Repo</a>
