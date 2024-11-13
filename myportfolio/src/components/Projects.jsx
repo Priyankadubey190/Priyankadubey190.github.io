bf368496e10adb2c044adae42a0d17d49ae6854b
@priyankadubey0811 <priyankadubey0811@gmail.com>
2024-11-13 12:40:34 +0530

    project pic added

--- myportfolio/src/components/Projects.jsx
+++ myportfolio/src/components/Projects.jsx
@@ -1,41 +1,111 @@
-import React from 'react'
+import React from "react";
 import style from "./project.module.css";
-const Projects =()=> {
+const Projects = () => {
   return (
     <div id="projects" className={style.procon}>
-       <center className={style.maincen}>Projects</center>
-       <div className={style.myproject}>
+      <center className={style.maincen}>Projects</center>
+      <div className={style.myproject}>
         <div>
-            
-            <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1527).png" alt="" width={"100%"}/>
-            <center className={style.cen}>Solo e-commerce</center>
-            <p> This is a solo e-commerce website. In this website I used React for frontend for backend mongoDB express.</p>
-            <div>
+          <img
+            src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1527).png"
+            alt=""
+            width={"100%"}
+          />
+          <center className={style.cen}>Solo e-commerce</center>
+          <p>
+            {" "}
+            This is a solo e-commerce website. In this website I used React for
+            frontend for backend mongoDB express.
+          </p>
+          <div>
             <a href="https://my-solo-full-ecommerce.netlify.app/">Live Site</a>
-            <a href="https://github.com/Priyankadubey190/full-crud-application/tree/main/full-stack-app" className={style.apro}>View Repo</a>
-            </div>
+            <a
+              href="https://github.com/Priyankadubey190/full-crud-application/tree/main/full-stack-app"
+              className={style.apro}
+            >
+              View Repo
+            </a>
+          </div>
         </div>
         <div>
-        <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1529).png" alt=""  width={"100%"}/>
-            <center className={style.cen}>Animoto-Clone</center>
-            <p>This is solo project in this website for front end I used HTML css javaScript and for backend I used JSON server.</p>
-            <div>
+          <img
+            src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1529).png"
+            alt=""
+            width={"100%"}
+          />
+          <center className={style.cen}>Animoto-Clone</center>
+          <p>
+            This is solo project in this website for front end I used HTML css
+            javaScript and for backend I used JSON server.
+          </p>
+          <div>
             <a href="https://animoto-website.netlify.app/">Live Site</a>
-            <a href="https://github.com/Priyankadubey190/innate-earth-4569" className={style.apro}>View Repo</a>
-            </div>
+            <a
+              href="https://github.com/Priyankadubey190/innate-earth-4569"
+              className={style.apro}
+            >
+              View Repo
+            </a>
+          </div>
         </div>
         <div>
-        <img src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1532).png" alt=""  width={"100%"}/>
-            <center className={style.cen}>Nature's Basket-Clone</center>
-            <p>This is the clone of nature's basket website, we can use this website to buy fresh food item.</p>
-            <div>
+          <img
+            src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/Screenshot%20(1532).png"
+            alt=""
+            width={"100%"}
+          />
+          <center className={style.cen}>Nature's Basket-Clone</center>
+          <p>
+            This is the clone of nature's basket website, we can use this
+            website to buy fresh food item.
+          </p>
+          <div>
             <a href="https://naturesbaskett.vercel.app/">Live Site</a>
-            <a href="https://github.com/JatinKhatter07/wiggly-bucket-6915" className={style.apro}>View Repo</a>
-            </div>
+            <a
+              href="https://github.com/JatinKhatter07/wiggly-bucket-6915"
+              className={style.apro}
+            >
+              View Repo
+            </a>
+          </div>
         </div>
-       </div>
-
+        <div>
+          <img
+            src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/housedates.png"
+            alt=""
+            width={"100%"}
+          />
+          <center className={style.cen}>Nature's Basket-Clone</center>
+          <p>
+            This website, built on Next.js, allows users to browse houses
+            available for sale and rent. Users can directly purchase or rent
+            properties, with a built-in chat feature enabling communication
+            between property owners and potential customers.
+          </p>
+          <div>
+            <a href="https://housedates-35464.web.app/">Live Site</a>
+          </div>
+        </div>
+        <div>
+          <img
+            src="https://github.com/Priyankadubey190/Priyankadubey190.github.io/raw/master/myportfolio/public/webisabi.png"
+            alt=""
+            width={"100%"}
+          />
+          <center className={style.cen}>Nature's Basket-Clone</center>
+          <p>
+            This e-commerce website, built with Next.js, Tailwind CSS, and RTK
+            Query, provides a seamless platform for users to purchase articles
+            online. The site leverages functional components for a modern,
+            efficient design. Tailwind CSS ensures a responsive and visually
+            appealing user interface.
+          </p>
+          <div>
+            <a href="https://wabisabiproject.com/">Live Site</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
-export default Projects
+  );
+};
+export default Projects;
